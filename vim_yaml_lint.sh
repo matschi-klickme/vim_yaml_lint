7d0e498950629656be02f52838b519ca6b975ad2
Michael Thaler <michael.thaler@conesphere.com>
2019-05-29 15:44:26 +0200

    using right directory now

--- vim_yaml_lint.sh
+++ vim_yaml_lint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-x-terminal-emulator -e watch -n 2 ./yamllint_filtered/yamllint.filtered.sh $1 & lintermonittermpid=$! ; vim $1 && kill $lintermonittermpid
+scriptdir="$(dirname "$(readlink -f "$0")")" #Directory von aktuell ausgeführtem Script ermitteln
+x-terminal-emulator -e watch -n 2 $scriptdir/yamllint_filtered/yamllint.filtered.sh $1 & lintermonittermpid=$! ; vim $1 && kill $lintermonittermpid
 
