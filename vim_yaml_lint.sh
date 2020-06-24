bed491d3474862b68a88cfe2aa5a4222b8db2e4c
Michael Thaler <michael.thaler@conesphere.com>
2020-06-24 12:12:57 +0200

    i changed stuff

--- vim_yaml_lint.sh
+++ vim_yaml_lint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 scriptdir="$(dirname "$(readlink -f "$0")")" #Directory von aktuell ausgeführtem Script ermitteln
-terminator -x  watch -n 2 yamllint $1 & lintermonittermpid=$! ; vim $1 && kill $lintermonittermpid
+gnome-terminal --  watch -n 2 yamllint $1 & lintermonittermpid=$! ; vim $1 && kill $lintermonittermpid
 
